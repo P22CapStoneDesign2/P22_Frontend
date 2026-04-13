0f3464abae71d40ec326b43bd1033b1bf56a9674
SeYeong Jung <lux020329@gmail.com>
2026-04-13 15:12:25 +0900

    Professor 삭제 및 LessonScript 추가 #1

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//PROFESSOR 화면 띄우고 싶을 때
-import Professor from './Professor.jsx'
+// 교안(LessonScript) 화면 띄우고 싶을 때
+import LessonScript from './LessonScript.jsx'
 
 //퀴즈 화면 띄우고 싶을 때
 // import App from './App.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* PROFESSOR 화면 띄우고 싶을 때 */}
-    <Professor />
+    {/* 교안(LessonScript) 화면 띄우고 싶을 때 */}
+    <LessonScript />
     
     {/* 퀴즈 화면 띄우고 싶을 때 */}
     {/* <App /> */}
